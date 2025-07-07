ffa1ee5f4e271ba86bc9d06b7fdb3c4d8080b777
SurfFounder <realsurfer000@gmail.com>
2025-07-07 11:05:51 +0300

    <изменения в 2-3 словах>

--- frontend/my-tonconnect-code.js
+++ frontend/my-tonconnect-code.js
@@ -1,4 +1,3 @@
-// Подключение TON Connect UI
 const btn = document.getElementById('ton-connect-btn');
 const walletDiv = document.getElementById('wallet-address');
 
@@ -6,7 +5,7 @@ let tonConnectUI;
 
 btn.onclick = () => {
     if (!tonConnectUI) {
-        tonConnectUI = new TonConnectUI.TonConnectUI({
+        tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
             manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json',
         });
         tonConnectUI.uiOptions = { language: 'ru' };
