fb7a9e80be35fcef52a5dc5c8268ace89f4933e2
SurfFounder <realsurfer000@gmail.com>
2025-07-07 10:51:31 +0300

    Fix TonConnectUI loading order for browser

--- frontend/tonconnect.js
+++ frontend/tonconnect.js
@@ -2,15 +2,11 @@
 const btn = document.getElementById('ton-connect-btn');
 const walletDiv = document.getElementById('wallet-address');
 
-btn.onclick = async () => {
-    if (window.TonConnect) {
-        // Уже инициализировано
-        return;
-    }
-    const script = document.createElement('script');
-    script.src = 'https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js';
-    script.onload = () => {
-        const tonConnectUI = new TonConnectUI.TonConnectUI({
+let tonConnectUI;
+
+btn.onclick = () => {
+    if (!tonConnectUI) {
+        tonConnectUI = new TonConnectUI.TonConnectUI({
             manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json',
         });
         tonConnectUI.uiOptions = { language: 'ru' };
@@ -21,7 +17,6 @@ btn.onclick = async () => {
                 walletDiv.innerText = '';
             }
         });
-        tonConnectUI.openModal();
-    };
-    document.body.appendChild(script);
+    }
+    tonConnectUI.openModal();
 }; 
