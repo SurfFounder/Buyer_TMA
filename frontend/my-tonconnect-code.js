b0c47259221d83e9d70dd688b7719dd0cc87bbcf
SurfFounder <realsurfer000@gmail.com>
2025-07-07 11:21:30 +0300

    Отображение USDT баланса

--- frontend/my-tonconnect-code.js
+++ frontend/my-tonconnect-code.js
@@ -3,15 +3,28 @@ const walletDiv = document.getElementById('wallet-address');
 
 let tonConnectUI;
 
+async function getUSDTBalance(address) {
+    const response = await fetch(`https://tonapi.io/v2/accounts/${address}/jettons`);
+    const data = await response.json();
+    const usdtJettonAddress = "EQCkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c";
+    const usdt = data.balances.find(j => j.jetton.address === usdtJettonAddress);
+    if (usdt) {
+        return (parseInt(usdt.balance) / 1e9).toLocaleString('en-US', {maximumFractionDigits: 2});
+    }
+    return "0";
+}
+
 btn.onclick = () => {
     if (!tonConnectUI) {
         tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
             manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json',
         });
         tonConnectUI.uiOptions = { language: 'ru' };
-        tonConnectUI.onStatusChange(wallet => {
+        tonConnectUI.onStatusChange(async wallet => {
             if (wallet && wallet.account) {
-                walletDiv.innerText = 'Адрес: ' + wallet.account.address;
+                walletDiv.innerText = 'Адрес: ' + wallet.account.address + '\nЗагрузка баланса...';
+                const balance = await getUSDTBalance(wallet.account.address);
+                walletDiv.innerText = `Адрес: ${wallet.account.address}\nUSDT: ${balance}`;
             } else {
                 walletDiv.innerText = '';
             }
