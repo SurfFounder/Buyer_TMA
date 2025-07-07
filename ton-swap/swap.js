1f0226f646b51566645895d41015426404d2315b
SurfFounder <realsurfer000@gmail.com>
2025-07-07 19:03:32 +0300

    swap

--- ton-swap/swap.js
+++ ton-swap/swap.js
@@ -1,5 +1,6 @@
 import { TonClient, toNano } from "@ton/ton";
 import { DEX, pTON } from "@ston-fi/sdk";
+import fs from "fs";
 
 const userWalletAddress = process.argv[2];
 const offerAmount = process.argv[3];
@@ -18,21 +19,31 @@ const proxyTon = pTON.v2_1.create(
   "kQACS30DNoUQ7NfApPvzh7eBmSZ9L4ygJ-lkNWtba8TQT-Px" // pTON v2.1.0
 );
 
+function logToFile(message) {
+  fs.appendFileSync("swap.log", `[${new Date().toISOString()}] ${message}\n`);
+}
+
 async function main() {
   console.log("userWalletAddress:", userWalletAddress);
   console.log("offerAmount:", offerAmount);
-  const txParams = await router.getSwapTonToJettonTxParams({
-    userWalletAddress: "UQCQNMglcI2q0aSMGTubOfCxpwaMM2q3Yyukql73euO2kOBw",
-    proxyTon,
-    offerAmount: toNano(offerAmount),
-    askJettonAddress: "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs",
-    minAskAmount: "1",
-    referralAddress: "",
-    referralValue: 0,
-    queryId: 12345,
-  });
-  // Выводим параметры в формате JSON для backend
-  console.log(JSON.stringify(txParams));
+  logToFile("userWalletAddress: " + userWalletAddress);
+  logToFile("offerAmount: " + offerAmount);
+  try {
+    const txParams = await router.getSwapTonToJettonTxParams({
+      userWalletAddress,
+      proxyTon,
+      offerAmount: toNano(offerAmount),
+      askJettonAddress: "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs",
+      minAskAmount: "1",
+      queryId: 12345,
+    });
+    logToFile("Swap params успешно получены");
+    // Выводим параметры в формате JSON для backend
+    console.log(JSON.stringify(txParams));
+  } catch (e) {
+    logToFile("Ошибка: " + e.toString());
+    throw e;
+  }
 }
 
 main(); 
